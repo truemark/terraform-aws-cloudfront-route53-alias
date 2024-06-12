8f2a8cdc16e019461a7db04f169e5e0f59ef3d0a
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-06-12 12:28:40 -0600

    Fixing formatting

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ data "aws_route53_zone" "this" {
 }
 
 resource "aws_route53_record" "this" {
-  for_each = {for a in var.aliases: a => a}
-  name = each.key
-  type = "A"
-  zone_id = data.aws_route53_zone.this.zone_id
+  for_each = { for a in var.aliases : a => a }
+  name     = each.key
+  type     = "A"
+  zone_id  = data.aws_route53_zone.this.zone_id
   alias {
     evaluate_target_health = false
-    name = data.aws_cloudfront_distribution.this.domain_name
-    zone_id = data.aws_cloudfront_distribution.this.hosted_zone_id
+    name                   = data.aws_cloudfront_distribution.this.domain_name
+    zone_id                = data.aws_cloudfront_distribution.this.hosted_zone_id
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "zone_name" {
   description = "Name of the route53 zone"
-  type = string
+  type        = string
 }
 
 variable "aliases" {
   description = "List of aliases to create in the zone"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "distribution_id" {
   description = "ID of the Cloudfront distribution"
-  type = string
+  type        = string
 }
